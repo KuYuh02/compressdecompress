e19a94d7954f5b6f96bdf8b2137f400d8f47c982
Martin De Guzman <57105334+KuYuh02@users.noreply.github.com>
2025-02-01 17:18:03 -0800

    Update compress.cpp
    huffman

--- compress.cpp
+++ compress.cpp
@@ -1,60 +1,101 @@
 #include <iostream>
+#include <queue>
 #include <unordered_map>
 #include <vector>
 #include <bitset>
 #include <sstream>
 
-std::string compress(const std::string& source) {
-    std::unordered_map<std::string, int> dictionary;
-    std::vector<int> compressed;
-    std::string w;
-    int dictSize = 256;
-    for (char c : source) {
-        std::string wc = w + c;
-        if (dictionary.count(wc)) {
-            w = wc;
-        } else {
-            compressed.push_back(dictionary.count(w) ? dictionary[w] : w[0]);
-            dictionary[wc] = dictSize++;
-            w = c;
-        }
+struct Node {
+    char ch;
+    int freq;
+    Node* left;
+    Node* right;
+    Node(char c, int f) : ch(c), freq(f), left(nullptr), right(nullptr) {}
+};
+
+struct Compare {
+    bool operator()(Node* a, Node* b) {
+        return a->freq > b->freq;
     }
-    if (!w.empty())
-        compressed.push_back(dictionary.count(w) ? dictionary[w] : w[0]);
+};
+
+void buildHuffmanTree(const std::string& source, std::unordered_map<char, std::string>& huffmanCodes, std::string& encodedString) {
+    std::unordered_map<char, int> freqMap;
+    for (char ch : source) {
+        freqMap[ch]++;
+    }
     
-    std::ostringstream oss;
-    for (int code : compressed) {
-        oss << code << " ";
+    std::priority_queue<Node*, std::vector<Node*>, Compare> minHeap;
+    for (const auto& pair : freqMap) {
+        minHeap.push(new Node(pair.first, pair.second));
     }
-    return oss.str();
+    
+    while (minHeap.size() > 1) {
+        Node* left = minHeap.top(); minHeap.pop();
+        Node* right = minHeap.top(); minHeap.pop();
+        Node* newNode = new Node('\0', left->freq + right->freq);
+        newNode->left = left;
+        newNode->right = right;
+        minHeap.push(newNode);
+    }
+    
+    Node* root = minHeap.top();
+    std::function<void(Node*, std::string)> encode = [&](Node* node, std::string str) {
+        if (!node) return;
+        if (node->ch != '\0') {
+            huffmanCodes[node->ch] = str;
+        }
+        encode(node->left, str + "0");
+        encode(node->right, str + "1");
+    };
+    
+    encode(root, "");
+    for (char ch : source) {
+        encodedString += huffmanCodes[ch];
+    }
+}
+
+std::string compress(const std::string& source) {
+    std::unordered_map<char, std::string> huffmanCodes;
+    std::string encodedString;
+    buildHuffmanTree(source, huffmanCodes, encodedString);
+    
+    std::string header;
+    for (const auto& pair : huffmanCodes) {
+        header += pair.first + std::to_string(pair.second.size()) + pair.second;
+    }
+    
+    return header + "#" + encodedString;
 }
 
 std::string decompress(const std::string& source) {
-    std::unordered_map<int, std::string> dictionary;
-    for (int i = 0; i < 256; i++) {
-        dictionary[i] = std::string(1, i);
+    std::unordered_map<std::string, char> reverseHuffmanCodes;
+    size_t pos = source.find('#');
+    if (pos == std::string::npos) return "";
+    
+    std::string header = source.substr(0, pos);
+    std::string encodedData = source.substr(pos + 1);
+    
+    size_t i = 0;
+    while (i < header.size()) {
+        char ch = header[i];
+        i++;
+        size_t len = header[i] - '0';
+        i++;
+        std::string code = header.substr(i, len);
+        reverseHuffmanCodes[code] = ch;
+        i += len;
     }
     
-    std::istringstream iss(source);
-    std::vector<int> compressed;
-    int value;
-    while (iss >> value) {
-        compressed.push_back(value);
-    }
-    
-    std::string w(1, compressed[0]);
-    std::string decompressed = w;
-    int dictSize = 256;
-    for (size_t i = 1; i < compressed.size(); i++) {
-        std::string entry;
-        if (dictionary.count(compressed[i])) {
-            entry = dictionary[compressed[i]];
-        } else if (compressed[i] == dictSize) {
-            entry = w + w[0];
+    std::string decoded;
+    std::string current;
+    for (char bit : encodedData) {
+        current += bit;
+        if (reverseHuffmanCodes.count(current)) {
+            decoded += reverseHuffmanCodes[current];
+            current.clear();
         }
-        decompressed += entry;
-        dictionary[dictSize++] = w + entry[0];
-        w = entry;
     }
-    return decompressed;
+    
+    return decoded;
 }
