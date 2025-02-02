241915350788e2d6e6cf18c3293e1ec53d8ad5c8
Martin De Guzman <57105334+KuYuh02@users.noreply.github.com>
2025-02-01 17:39:54 -0800

    Update compress.cpp

--- compress.cpp
+++ compress.cpp
@@ -1,27 +1,5 @@
 #include <string>
-#include <unordered_map>
-#include <queue>
-#include <vector>
-#include <iostream>
-#include <bitset>
 
-class HuffmanNode {
-public:
-    char ch;
-    int freq;
-    HuffmanNode* left;
-    HuffmanNode* right;
-
-    HuffmanNode(char character, int frequency) 
-        : ch(character), freq(frequency), left(nullptr), right(nullptr) {}
-
-    // Comparator for priority queue (min-heap)
-    struct Compare {
-        bool operator()(HuffmanNode* a, HuffmanNode* b) {
-            return a->freq > b->freq;
-        }
-    };
-};
 std::string compress(const std::string& source) {
    
 }
