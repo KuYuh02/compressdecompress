d7552e6e9dd14e4fb69a790a9a3d40f04ef9cf0d
Martin De Guzman <57105334+KuYuh02@users.noreply.github.com>
2025-02-01 18:22:20 -0800

    Update compress.cpp

--- compress.cpp
+++ compress.cpp
@@ -1,95 +1,41 @@
-#include <iostream>
-#include <queue>
+#include <string>
 #include <unordered_map>
-#include <vector>
-#include <bitset>
-#include <sstream>
 
-std::string compress(const std::string& source) {
-    std::unordered_map<char, int> freqMap;
-    for (char ch : source) {
-        freqMap[ch]++;
+using namespace std;
+
+// Function to create a frequency table for characters in the input string
+unordered_map<char, int> build_frequency_table(const string& source) {
+    unordered_map<char, int> freq_table;
+    for (char c : source) {
+        freq_table[c]++;
     }
-    
-    struct Node {
-        char ch;
-        int freq;
-        Node* left;
-        Node* right;
-        Node(char c, int f) : ch(c), freq(f), left(nullptr), right(nullptr) {}
-    };
-    
-    struct Compare {
-        bool operator()(Node* a, Node* b) {
-            return a->freq > b->freq;
-        }
-    };
-    
-    std::priority_queue<Node*, std::vector<Node*>, Compare> minHeap;
-    for (const auto& pair : freqMap) {
-        minHeap.push(new Node(pair.first, pair.second));
-    }
-    
-    while (minHeap.size() > 1) {
-        Node* left = minHeap.top(); minHeap.pop();
-        Node* right = minHeap.top(); minHeap.pop();
-        Node* newNode = new Node('\0', left->freq + right->freq);
-        newNode->left = left;
-        newNode->right = right;
-        minHeap.push(newNode);
-    }
-    
-    Node* root = minHeap.top();
-    std::unordered_map<char, std::string> huffmanCodes;
-    std::function<void(Node*, std::string)> encode = [&](Node* node, std::string str) {
-        if (!node) return;
-        if (node->ch != '\0') {
-            huffmanCodes[node->ch] = str;
-        }
-        encode(node->left, str + "0");
-        encode(node->right, str + "1");
-    };
-    encode(root, "");
-    
-    std::ostringstream header;
-    header << huffmanCodes.size() << " ";
-    for (const auto& pair : huffmanCodes) {
-        header << pair.first << " " << pair.second << " ";
-    }
-    
-    std::ostringstream encodedString;
-    for (char ch : source) {
-        encodedString << huffmanCodes[ch];
-    }
-    
-    return header.str() + "#" + encodedString.str();
+    return freq_table;
 }
 
-std::string decompress(const std::string& source) {
-    size_t pos = source.find('#');
-    if (pos == std::string::npos) return "";
+// Function to compress a string using a frequency table
+string compress(const string& source) {
+    unordered_map<char, int> freq_table = build_frequency_table(source);
     
-    std::istringstream headerStream(source.substr(0, pos));
-    std::string encodedData = source.substr(pos + 1);
-    
-    std::unordered_map<std::string, char> reverseHuffmanCodes;
-    size_t tableSize;
-    headerStream >> tableSize;
-    for (size_t i = 0; i < tableSize; i++) {
-        char ch;
-        std::string code;
-        headerStream >> ch >> code;
-        reverseHuffmanCodes[code] = ch;
+    string compressed;
+    for (char c : source) {
+        compressed += c + to_string(freq_table[c]);
     }
-    
-    std::string decoded, current;
-    for (char bit : encodedData) {
-        current += bit;
-        if (reverseHuffmanCodes.count(current)) {
-            decoded += reverseHuffmanCodes[current];
-            current.clear();
+    return compressed;
+}
+
+// Function to decompress a compressed string
+string decompress(const string& compressed) {
+    string decompressed;
+    for (int i = 0; i < compressed.size(); i++) {
+        char ch = compressed[i];
+        int count = 0;
+        while (isdigit(compressed[++i])) {
+            count = count * 10 + (compressed[i] - '0');
+        }
+        i--; // Adjust index to point back to the character
+        for (int j = 0; j < count; j++) {
+            decompressed += ch;
         }
     }
-    
-    return decoded;
+    return decompressed;
 }
